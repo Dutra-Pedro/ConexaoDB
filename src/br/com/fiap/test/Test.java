3c8623dda72e080be8fd8215a84ae0a3c8b74c01
logonrmlocal <dutra.pedrosousa@gmail.com>
2023-04-25 09:19:16 -0300

    oi

---
+++ src/br/com/fiap/test/Test.java
@@ -0,0 +1,11 @@
+package br.com.fiap.test;
+
+import br.com.fiap.connection.ConnectionFactory;
+
+
+public class Test {
+    public static void main(String[] args) {
+
+        new ConnectionFactory().conectar();
+    }
+}
