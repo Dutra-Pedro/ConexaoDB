05ffdde094167c8cd81bad09258d906a9df62422
logonrmlocal <dutra.pedrosousa@gmail.com>
2023-04-27 11:49:04 -0300

    n funfo n

--- src/br/com/fiap/DAO/UsuarioDAO.java
+++ src/br/com/fiap/DAO/UsuarioDAO.java
@@ -1,9 +1,6 @@
 package br.com.fiap.DAO;
-
-
 import br.com.fiap.connection.ConnectionFactory;
 import br.com.fiap.model.Usuario;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -11,12 +8,12 @@ import java.sql.SQLException;
 public class UsuarioDAO {
     private Connection conexao;
 
-    public UsuarioDAO(Connection conexao) {
+    public UsuarioDAO() {
         this.conexao = new ConnectionFactory().conectar();
     }
 
     public void insert(Usuario usuario) throws SQLException {
-        String sql = "INSERT INTO USUARIO (id_usuario, nome, email, senha, dt_registro) values (?,?,?,?,?)";
+        String sql = "INSERT INTO USUARIO(id_usuario, nome, email, senha, dt_registro) VALUES (?,?,?,?,?)";
         PreparedStatement stmt = conexao.prepareStatement(sql);
         stmt.setInt(1,usuario.getId());
         stmt.setString(2, usuario.getNome());
--- src/br/com/fiap/test/Test.java
+++ src/br/com/fiap/test/Test.java
@@ -1,10 +1,17 @@
 package br.com.fiap.test;
-
+import br.com.fiap.DAO.UsuarioDAO;
 import br.com.fiap.connection.ConnectionFactory;
+import br.com.fiap.model.Usuario;
+import java.sql.SQLException;
 
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         new ConnectionFactory().conectar();
+        UsuarioDAO DAO = new UsuarioDAO();
+        Usuario user = new Usuario(1,"Pedro", "pedro@gmail.com","12345");
+
+        DAO.insert(user);
+
     }
 }
